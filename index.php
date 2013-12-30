550159195cbc19c9f8ef391afffa4b79976b237e
Demidenko I <deila.coding@gmail.com>
2013-12-30 09:15:54 +0200

    added lab5 and lab6 to index

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
 <li><a href="/lab1">Вход</a> | <a href="https://github.com/deila/internet-labs/tree/master/lab1">Код</a></li>
 <li><a href="/lab2/app">Вход</a> | <a href="/lab2/pgadmin">pgadmin</a> | <a href="https://github.com/deila/internet-labs/tree/master/lab2">Код</a></li>
 <li><a href="/lab3/src">Вход</a> | <a href="https://github.com/deila/internet-labs/tree/master/lab3/src/index.php">Код</a></li>
-<li><a href="/lab4/app">Вход [60%] </a> | <a href="https://github.com/deila/internet-labs/tree/master/lab4/app">Код</a></li>
+<li><a href="/lab4/app">Вход</a> | <a href="https://github.com/deila/internet-labs/tree/master/lab4/app">Код</a></li>
+<li><a href="http://pure-sierra-3447.herokuapp.com/" target="_blank">Вход (в новой вкладке)</a> | <a href="https://github.com/deila/internet-lab5/tree/master/app">Код</a></li>
+<li><a href="http://bookstorage.meteor.com/" target="_blank">Вход (в новой вкладке)</a> | <a href="https://github.com/deila/internet-lab6/tree/master/app">Код</a></li>
 </ol>
 
